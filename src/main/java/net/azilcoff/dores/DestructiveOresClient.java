a8d9f775eb24415faafc3bf5a8d6e657c30a40f9
azilcoff <azizgenshin&@gmail.com>
2025-11-12 14:08:28 +0300

    Initial commit

---
+++ src/main/java/net/azilcoff/dores/DestructiveOresClient.java
@@ -0,0 +1,10 @@
+package net.azilcoff.dores;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DestructiveOresClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/azilcoff/dores/DestructiveOresDataGenerator.java
@@ -0,0 +1,11 @@
+package net.azilcoff.dores;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DestructiveOresDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
